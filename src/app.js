23eac81ad0484bf0ff0322435e293dc836391811
Humberto Carmona <carmona@fisica.ufc.br>
2024-07-06 18:04:58 -0300

    serve html form

--- src/app.js
+++ src/app.js
@@ -1,10 +1,29 @@
 import express from 'express';
-import './dbSetup.js';
-import './dbAddUsers.js';
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+// import './dbSetup.js';
+// import './dbAddUsers.js';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 
 const app = express();
-app.listen(3000, ()=>{
-    console.log('server is running on port 3000');
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+app.get("/addUserForm", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "addUser.html"));
 });
 
-app.use(express.static('public'))
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
